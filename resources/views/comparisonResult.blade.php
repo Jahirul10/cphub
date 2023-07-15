abc986b4fe2f4336fda90dc6ba80995f65bcb73e
Jahirul10 <mdjahirul495@gmail.com>
2023-07-15 20:43:48 +0600

    charts

--- resources/views/comparisonResult.blade.php
+++ resources/views/comparisonResult.blade.php
@@ -4,53 +4,53 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Comparison Result</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title>Bar Chart Example</title>
+    <script src="{{ asset('js/Chart.min.js') }}"></script>
 </head>
 
 <body>
-    <h1>Hasibul</h1>
-
-    <!-- Your other content here -->
-
     <canvas id="barChart"></canvas>
 
     <script>
-        $(document).ready(function() {
-            var ctx = document.getElementById('barChart').getContext('2d');
-            var barChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-                    datasets: [{
-                        label: 'Bar Chart Example',
-                        data: [12, 19, 3, 5, 2],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.5)',
-                            'rgba(54, 162, 235, 0.5)',
-                            'rgba(255, 206, 86, 0.5)',
-                            'rgba(75, 192, 192, 0.5)',
-                            'rgba(153, 102, 255, 0.5)',
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                        ],
-                        borderWidth: 1,
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
+        // Get the chart canvas element
+        var ctx = document.getElementById('barChart').getContext('2d');
+
+        // Define the chart data
+        var chartData = {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+            datasets: [{
+                label: 'Bar Chart Example',
+                data: [12, 19, 3, 5, 2],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                ],
+                borderWidth: 1,
+            }]
+        };
+
+        // Create the bar chart
+        var barChart = new Chart(ctx, {
+            type: 'bar',
+            data: chartData,
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
                     }
                 }
-            });
+            }
         });
     </script>
 </body>
