15af5cc69efdd067d421f2deae7afcd0d44356ce
mohammadFromBengal <mohammadfrombengal@gmail.com>
2023-07-16 04:09:42 +0600

    search page with ajax completed

--- resources/views/publicSearch.blade.php
+++ resources/views/publicSearch.blade.php
@@ -74,7 +74,7 @@
                                 </form> <!-- Close the form element here -->
                             </div>
 
-                            <div class="d-none">
+                            <div id="submissionHistory" class="d-none">
                                 <hr>
                                 <h2 class="p-2">Submission Records</h2>
                                 <table class="table table-striped">
@@ -91,6 +91,10 @@
                                     <tbody id="submission_table">
                                     </tbody>
                                 </table>
+                                <!-- Pagination -->
+                                <nav>
+                                    <ul class="pagination justify-content-center" id="pagination"></ul>
+                                </nav>
                             </div>
 
                             <script>
@@ -110,15 +114,145 @@
                                             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                                         },
                                         data: {
-                                            codeforcesHandle: codeforcesHandle,
-                                            vjudgeHandle: vjudgeHandle,
-                                            spojHandle: spojHandle
+                                            _token: $('meta[name="csrf-token"]').attr('content'), // Add this line to include the CSRF token
+                                            codeforces: codeforcesHandle, // Modify the key to 'codeforces'
+                                            vjudge: vjudgeHandle, // Modify the key to 'vjudge'
+                                            spoj: spojHandle // Modify the key to 'spoj'
                                         },
                                         success: function(response) {
                                             console.log(response);
-                                            // Update the UI with the response data
-                                            // For example, update the submission table with response.submissions
-                                            // document.getElementById('submission_table').innerHTML = response.submissions;
+
+                                            // Unhide the table
+                                            document.getElementById('submissionHistory').classList.remove('d-none');
+
+
+                                            var tableBody = $('#submission_table');
+                                            tableBody.empty();
+
+                                            var currentPage = 1;
+                                            var rowsPerPage = 50;
+                                            var totalPages = Math.ceil(response.submissions.length / rowsPerPage);
+
+                                            console.log("check-before-function");
+
+                                            function showRows() {
+                                                var start = (currentPage - 1) * rowsPerPage;
+                                                var end = start + rowsPerPage;
+
+
+                                                tableBody.empty();
+
+                                                for (var i = start; i < end; i++) {
+                                                    if (i >= response.submissions.length) {
+                                                        break;
+                                                    }
+
+                                                    var submission = response.submissions[i];
+                                                    var row = $('<tr>');
+
+
+                                                    $('<td>').text(submission[0]).appendTo(row);
+                                                    $('<td>').text(submission[1]).appendTo(row);
+                                                    $('<td>').text(submission[2]).appendTo(row);
+                                                    $('<td>').text(submission[3]).appendTo(row);
+                                                    $('<td>').text(submission[4]).appendTo(row);
+                                                    $('<td>').text(submission[5]).appendTo(row);
+
+                                                    tableBody.append(row);
+                                                }
+                                            }
+
+                                            function updatePagination() {
+                                                var pagination = $('#pagination');
+                                                pagination.empty();
+
+                                                var firstButton = $('<li class="page-item"><a class="page-link" href="#">1</a></li>');
+                                                var prevButton = $('<li class="page-item"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>');
+                                                var nextButton = $('<li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>');
+                                                var lastButton = $('<li class="page-item"><a class="page-link" href="#">' + totalPages + '</a></li>');
+
+                                                firstButton.click(function(e) {
+                                                    e.preventDefault(); // Prevent the default behavior of the anchor tag
+                                                    if (currentPage !== 1) {
+                                                        currentPage = 1;
+                                                        showRows();
+                                                        updatePagination();
+                                                    }
+                                                });
+
+                                                prevButton.click(function(e) {
+                                                    e.preventDefault(); // Prevent the default behavior of the anchor tag
+                                                    if (currentPage > 1) {
+                                                        currentPage--;
+                                                        showRows();
+                                                        updatePagination();
+                                                    }
+                                                });
+
+                                                nextButton.click(function(e) {
+                                                    e.preventDefault(); // Prevent the default behavior of the anchor tag
+                                                    if (currentPage < totalPages) {
+                                                        currentPage++;
+                                                        showRows();
+                                                        updatePagination();
+                                                    }
+                                                });
+
+                                                lastButton.click(function(e) {
+                                                    e.preventDefault(); // Prevent the default behavior of the anchor tag
+                                                    if (currentPage !== totalPages) {
+                                                        currentPage = totalPages;
+                                                        showRows();
+                                                        updatePagination();
+                                                    }
+                                                });
+
+                                                pagination.append(firstButton);
+                                                pagination.append(prevButton);
+
+                                                var maxVisiblePages = 5; // Set the maximum number of visible page buttons
+                                                var startPage = Math.max(currentPage - Math.floor(maxVisiblePages / 2), 1);
+                                                var endPage = Math.min(startPage + maxVisiblePages - 1, totalPages);
+
+                                                // Adjust startPage when it is not within the valid range
+                                                if (endPage === totalPages && startPage > 1) {
+                                                    startPage = Math.max(endPage - maxVisiblePages + 1, 1);
+                                                }
+
+                                                if (startPage > 1) {
+                                                    pagination.append('<li class="page-item disabled"><a class="page-link" href="#">...</a></li>');
+                                                }
+
+                                                for (var i = startPage; i <= endPage; i++) {
+                                                    var pageButton = $('<li class="page-item"><a class="page-link" href="#">' + i + '</a></li>');
+                                                    pageButton.click(function(e) {
+                                                        e.preventDefault(); // Prevent the default behavior of the anchor tag
+                                                        currentPage = parseInt($(this).text());
+                                                        showRows();
+                                                        updatePagination();
+                                                    });
+
+                                                    // Add 'active' class to the current page button
+                                                    if (i === currentPage) {
+                                                        pageButton.addClass('active');
+                                                    }
+
+                                                    pagination.append(pageButton);
+                                                }
+
+                                                if (endPage < totalPages) {
+                                                    pagination.append('<li class="page-item disabled"><a class="page-link" href="#">...</a></li>');
+                                                }
+
+                                                pagination.append(nextButton);
+                                                pagination.append(lastButton);
+                                            }
+
+                                            showRows();
+                                            updatePagination();
+                                            console.log("check-last");
+
+
                                         },
                                         error: function(xhr, status, error) {
                                             console.log(error);
@@ -126,6 +260,7 @@
                                     });
                                 });
                             </script>
+
 
                         </div>
                     </div>
